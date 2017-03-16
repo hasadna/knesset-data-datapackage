e2561d0665cde8cf54ab329e77170d671047248c
Ori Hoch <ori@uumpa.com>
2017-03-16 14:40:47 +0200

    ensure correct order of resources in root datapackage

--- knesset_datapackage/root.py
+++ knesset_datapackage/root.py
@@ -1,15 +1,16 @@
 from knesset_datapackage.base import BaseDatapackage, DatapackageResourceLink
 from knesset_datapackage.resources.committees import CommitteesResource, CommitteeMeetingsResource, CommitteeMeetingProtocolsResource
 from knesset_datapackage.resources.members import MembersResource
+from collections import OrderedDict
 
 
 class RootDatapackage(BaseDatapackage):
 
     NAME = "knesset-data"
-    RESOURCES = {
-        "members": (MembersResource, {}),
+    RESOURCES = OrderedDict([
+        ("committees", (CommitteesResource, {"meetings_resource": DatapackageResourceLink("committee-meetings")})),
+        ("committee-meetings", (CommitteeMeetingsResource, {"protocols_resource": DatapackageResourceLink("committee-meetings-protocols")})),
+        ("committee-meetings-protocols", (CommitteeMeetingProtocolsResource, {})),
 
-        "committees": (CommitteesResource, {"meetings_resource": DatapackageResourceLink("committee-meetings")}),
-        "committee-meetings": (CommitteeMeetingsResource, {"protocols_resource": DatapackageResourceLink("committee-meetings-protocols")}),
-        "committee-meetings-protocols": (CommitteeMeetingProtocolsResource, {}),
-    }
+        ("members", (MembersResource, {})),
+    ])
